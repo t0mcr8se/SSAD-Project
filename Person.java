c4118cbed432f6d8d4a01e81324526000bd09012
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-10-06 20:45:17 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -4,14 +4,14 @@ class Person{
     protected String username, password, firstName, sureName; // TODO: make private
     // TODO: add name or smth
     int id;
-    Person(String username, String password, boolean isAdmin, int age, String firstName, String sureName){
+    Person(String username, String password, boolean isAdmin, int age, String firstName, String surName){
         // TODO: get user from the database if already exists, else generate new ID
         this.username = username;
         this.password = password;
         this.age = age;
         this.isAdmin = isAdmin;
         this.firstName = firstName;
-        this.sureName = sureName;
+        this.surName = surName;
     }
     Person(int id){
         // TODO: get person from the database
