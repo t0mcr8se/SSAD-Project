02d9988976244cf9a2147655ae278b488a57cb9c
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-10-06 21:02:24 +0300

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,9 +1,9 @@
 public class Company {
-    Stirng name,owner,foundingDate,grievances[];
-    int revenue;
-    Company(){
-
-
-    }
-    
+    protected String name, owner, grievances[];
+    protected int revenue;
+    Company(String name,String owner, String grievances[]) {
+        this.name = name;
+        this.owner = owner;
+        this.grievances = grievances;
+    }   
 }
