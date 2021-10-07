1baeb93c59ae51defa284e1f717875b71e3fb97d
Mosab Mohamed <44644160+IVIosab@users.noreply.github.com>
2021-10-07 17:16:25 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,9 +1,10 @@
-class Person{
+class Person {
     protected int age;
     protected boolean isAdmin;
     protected String username, password, firstName, surName;
-    int id;
-    Person(String username, String password, boolean isAdmin, int age, String firstName, String surName){
+    protected int id; // each person should have a unique ID when created
+
+    Person(String username, String password, boolean isAdmin, int age, String firstName, String surName) {
         // TODO: get user from the database if already exists, else generate new ID
         this.username = username;
         this.password = password;
@@ -12,8 +13,20 @@ class Person{
         this.firstName = firstName;
         this.surName = surName;
     }
-    Person(int id){
+
+    /**
+     * this is used to fetch the person from the database based on his/her ID
+     * @param id
+     */
+    Person(int id) {
         // TODO: get person from the database
     }
-    void login(String username, String password){}
+
+    /**
+     * this method is used to login into the system
+     * @param username
+     * @param password
+     */
+    void login(String username, String password) {
+    }
 }
