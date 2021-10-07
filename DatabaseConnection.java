ef4fb9058ba7d20fb382df27a9c342799f2024ab
Mosab Mohamed <44644160+IVIosab@users.noreply.github.com>
2021-10-07 17:15:14 +0300

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,25 +1,42 @@
 public class DatabaseConnection {
     private static DatabaseConnection db;
 
-    private DatabaseConnection() {}
+    private DatabaseConnection() {
+    }
 
-    public DatabaseConnection getDB(){
-        if(db == null){
+    /**
+     * Singleton design pattern for the Database connection to control the load balance, unnecessary connections, shared db connection management... etc
+     *
+     * @return
+     */
+    public DatabaseConnection getDB() {
+        if (db == null) {
             db = new DatabaseConnection();
         }
         return db;
     }
 
-    void write(String sql){
+    /**
+     * this method is used to add data to the database
+     *
+     * @param sql
+     */
+    void write(String sql) {
         System.out.println("DATA succesfully written to the database!");
     }
 
-    String read(String sql){
+    /**
+     * this method is used to retrieve data from the database
+     *
+     * @param sql
+     * @return
+     */
+    String read(String sql) {
         // TODO: return smth
         System.out.println("DATA succesfully read from database!");
         return "";
 
     }
 
-    
+
 }
