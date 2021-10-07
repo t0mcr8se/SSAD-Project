788ace77b84674ce735200125adb216fb3299591
Mosab Mohamed <44644160+IVIosab@users.noreply.github.com>
2021-10-07 17:14:43 +0300

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,9 +1,10 @@
 public class Company {
     protected String name, owner, grievances[];
     protected int revenue;
-    Company(String name,String owner, String grievances[]) {
+
+    Company(String name, String owner, String grievances[]) {
         this.name = name;
         this.owner = owner;
         this.grievances = grievances;
-    }   
+    }
 }
