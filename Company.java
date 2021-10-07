0b53cf3ba1fa1f6b116a39a8c71c2fd45252952e
t0mcr8se <husamirami@gmail.com>
2021-10-07 20:38:57 +0300

    added company id

--- Company.java
+++ Company.java
@@ -1,13 +1,22 @@
-public class Company {
-    protected String name, owner, grievances[];
-    protected int revenue, id;
+import java.util.ArrayList;
 
-    Company(String name, String owner, String grievances[]) {
+public class Company {
+    protected String name, owner;
+    protected ArrayList<String> grievances; // Initially empty
+    protected int revenue, id;
+    private static int cnt = 0;
+
+    Company(String name, String owner) {
         this.name = name;
         this.owner = owner;
-        this.grievances = grievances;
+        id = cnt++;
     }
+
     Company(int id){
         // TODO: get company by id from the database
+    }
+
+    int getId(){
+        return this.id;
     }
 }
