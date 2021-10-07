15013ad086d5cad06aee1a33c88d65cd40107c0c
Mosab Mohamed <44644160+IVIosab@users.noreply.github.com>
2021-10-07 17:14:27 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,12 +1,43 @@
-public class Admin extends Person{
+public class Admin extends Person {
     // TODO: maybe Company company;
-    Admin(String username, String password, boolean isAdmin, int age, String firstName, String surName){
+    Admin(String username, String password, boolean isAdmin, int age, String firstName, String surName) {
         super(username, password, isAdmin, age, firstName, surName);
     }
 
-    void changeCompanyInfo(String info){}
-    void leaveManagement(){}
-    void changePayment(){}
-    void addEmplyee(String username, String password){}
-    void addEmployeeDetails(int id, Details details){}
+    /**
+     * this method is used to edit the company information
+     *
+     * @param info
+     */
+    void changeCompanyInfo(String info) { //what info does it change exactly ?
+    }
+
+    /**
+     * this method is used to manage the leave status for employees
+     */
+    void leaveManagement() {
+    }
+
+    /**
+     * this method is used to change the salary for employees
+     */
+    void changePayment() {
+    }
+
+    /**
+     * this method is used to add an employee to the database
+     *
+     * @param username
+     * @param password
+     */
+    void addEmplyee(String username, String password) {
+    }
+
+    /**
+     * this method is used to add information for an employee
+     *
+     * @param id
+     */
+    void addEmployeeDetails(int id) { // what details exactly ?
+    }
 }
