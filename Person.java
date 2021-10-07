3e23c3fdc7b8a2af0c74556bd4dbbdba081649ce
Mosab Mohamed <44644160+IVIosab@users.noreply.github.com>
2021-10-07 16:54:55 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 class Person{
     protected int age;
     protected boolean isAdmin;
-    protected String username, password, firstName, sureName;
+    protected String username, password, firstName, surName;
     int id;
     Person(String username, String password, boolean isAdmin, int age, String firstName, String surName){
         // TODO: get user from the database if already exists, else generate new ID
