49c815001bf7d16a0294bc42f949f41c5150c8ca
t0mcr8se <husamirami@gmail.com>
2021-10-07 20:39:11 +0300

    added main

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(){
+        Company company = new Company("Innopolis", "Succi");
+        Admin admin = new Admin("admin", "admin", 18, "Smith", "Smith", company.getId());
+
+
+
+
+    }
+}
