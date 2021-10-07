6a99b12ada48cbff3eb8d2d7c9d9e65e6e4df24c
t0mcr8se <husamirami@gmail.com>
2021-10-07 20:29:44 +0300

    added Company(id) constructor to get company from DB

--- Company.java
+++ Company.java
@@ -1,10 +1,13 @@
 public class Company {
     protected String name, owner, grievances[];
-    protected int revenue;
+    protected int revenue, id;
 
     Company(String name, String owner, String grievances[]) {
         this.name = name;
         this.owner = owner;
         this.grievances = grievances;
+    }
+    Company(int id){
+        // TODO: get company by id from the database
     }
 }
